8fba756e217aba07f1aa634dfd1e7ec26653db61
Nicolas Arnold <narnold@cl.uni-heidelberg.de>
2023-05-21 22:33:21 +0200

    Add functional implementatino of matrix_add

--- algebra.py
+++ algebra.py
@@ -28,10 +28,22 @@ def scalar_mult(M, k):
     # TODO: Nicolas implement
     return [list(map(lambda t: (t[0], k * t[1]), M[i])) for i in range(len(M))]
 
+def add(M1: M, M2: M) -> (M, str):
+    """element-wise addition of two matrices. in functional style.
 
-def add(M1: M, M2: M) -> M:
-    # TODO: docstring
-    # TODO: Nicolas: implement
+    fp alternative approch using map:
+    >>> from operator import add
+    >>> list( map(add, list1, list2) )
+    [5, 7, 9]
+    """
+
+    if len(M1) != len(M2):
+        return ((), "Rank is not identified")
+    return ([sum(x) for x in zip(list1, list2)], "")
+     
+
+def add_iterative(M1: M, M2: M) -> M:
+    """element-wise addition of two matrices. naive iterative way."""
 
     assert len(M1) == len(M2) # extract to properties.py
 
@@ -42,16 +54,13 @@ def add(M1: M, M2: M) -> M:
         for col_index in range(len(M1[0])):
             row_sum.append(M1[row_index][col_index] + M2[row_index][col_index])
         M_sum.append(row_sum)
-
-    #if len(M1) != len(M2):
-    #    return ((), "Rank is not identified")
     return M_sum
 
 M1 = [[1,2,3], [3,2,1]]
 M2 = [[1,1,1], [0,0,0]]
 print(M1)
 print(M2)
-print(add(M1, M2))
+print(add_iterative(M1, M2))
 
 def mult(M1, M2):
     return
