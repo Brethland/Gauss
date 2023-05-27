5201df8f339dfba3bef130ce8c263920424c491f
Nicolas Arnold <narnold@cl.uni-heidelberg.de>
2023-05-27 17:06:04 +0200

    Remove recursive gauss sketch from gauss.py

--- gauss.py
+++ gauss.py
@@ -5,44 +5,6 @@ from functools import reduce
 # TODO: Add step-by-step application of the elementary matrix stack for demonstration
 
 
-def gauss_split(m : M, nowrow: int, nowcol: int) -> M:
-    """
-    0. Put all nullrows to the bottom
-    1. Establish a useful toprow by finding left-most pivot row. Swap if necessary
-
-    """
-    # TODO: establish base case handling!
-    n_rows = len(m)
-    n_cols = len(m[0])
-
-    # TODO: put to function, reestablish/update after any operation that can create 0-rows.
-    # 0. All nullrows to the bottom
-    n_zerorows = 0
-    for i, row in enumerate(m):
-        if is_nullrow(row):
-            m = S(n_cols, i, )
-            n_zerorows += 1
-    rank = n_rows - n_zerorows
-
-    
-    #    last_useful_row = rank
-
-    # 1. Establish a useful toprow
-    ## Skip zero cols:
-    while is_nullrow(col(nowcol)):
-           nowcol += 1
-    #if m[nowrow][0] == 0: #we need to swap
-    better_candidate = nowrow
-    for rowindex, value in enumerate(col(nowcol)):
-        pass
-    for rowindex in range()
-
-    return gauss_split()
-
-def gauss_split_go(m: M):
-    """run recursive gauss_split with initial values"""
-    return gauss_split(m, len(m), len(m[0]))
-
 def find_better_candidate(m : M, c: int, end_column: int, now_row: int) -> int:
     """
     Recursive function, going through the left-most non-zero column to find
